45e848c8435a692069b2af171c300b7e763cb6e5
Flynn McGouran-Collins <fmm944@student.bham.ac.uk>
2022-11-09 01:48:18 +0000

    TLS proxy working for TCP web traffic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use native_tls::Identity;
 use std::collections::VecDeque;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read, Write};
+use std::io::Read;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio_native_tls::{TlsAcceptor, TlsConnector, TlsStream};
+use colored::Colorize;
 
-const REPLACEMENTS: &'static [(&[u8], &[u8])] =
-    &[("Host: 127.0.0.:1443".as_bytes(), "WOLOLOLO".as_bytes())];
+const DOMAIN : &str = "www.google.com";
+const REPLACEMENTS: &'static [(&[u8], &[u8])] = &[("127.0.0.1:1444".as_bytes(), DOMAIN.as_bytes()),
+                                                  ("fflkskkk".as_bytes(), "W2113dsf".as_bytes())];
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -48,7 +50,7 @@ async fn handle_client(tls_stream_client: TlsStream<TcpStream>) {
             .unwrap(),
     );
 
-    let stream_out = TcpStream::connect("reddit.com:443").await.unwrap();
+    let stream_out = TcpStream::connect("www.google.com:443").await.unwrap();
     let tls_stream_server = connector
         .connect("googlasde.com", stream_out)
         .await
@@ -61,7 +63,7 @@ async fn handle_client(tls_stream_client: TlsStream<TcpStream>) {
         let mut outbuf: VecDeque<(u8, Vec<VecDeque<u8>>)> = VecDeque::new();
 
         loop {
-            let mut testbuf = vec![0u8; 1];
+            //let mut testbuf = vec![0u8; 1];
             let read = match client_read_tls.read_u8().await {
                 Ok(v) => v,
                 Err(e) => {
@@ -71,14 +73,21 @@ async fn handle_client(tls_stream_client: TlsStream<TcpStream>) {
                 }
             };
 
-            print!("{}", read as char);
-
-            //let running = outbuf.back().map(|(f, s)| s.clone()).unwrap_or_default();
-
             //Remove all elements that no longer match.
-            outbuf = outbuf.into_iter().map(|(u, v)| (u, v.into_iter().filter(|f| f.front() == Some(&read)).collect())).collect::<VecDeque<_>>();
+            outbuf = outbuf
+                .into_iter()
+                .map(|(u, v)| {
+                    (
+                        u,
+                        v.into_iter().filter(|f| f.front() == Some(&read)).collect(),
+                    )
+                })
+                .collect::<VecDeque<_>>();
 
-
+            //Detect a completion
+            let det = outbuf
+                .iter()
+                .any(|(a, b)| b.iter().any(|l| l.len() == 1 && l.contains(&read)));
 
             //Add new element to the queue
             let base = REPLACEMENTS.iter().map(|(f, s)| *f).collect::<Vec<_>>();
@@ -92,14 +101,55 @@ async fn handle_client(tls_stream_client: TlsStream<TcpStream>) {
             outbuf.push_back((read, newfilt));
 
             //Pop front off all remaining elements
-            outbuf.iter_mut().for_each(|(_, v)| v.iter_mut().for_each(|f| {f.pop_front();}));
+            outbuf.iter_mut().for_each(|(_, v)| {
+                v.iter_mut().for_each(|f| {
+                    f.pop_front();
+                })
+            });
 
-            //Print all front elements with no matches
-            println!("{:?}", outbuf);
+            //Rebuild the completion and edit the buffer.
+            if det {
+                let mut candidates = REPLACEMENTS
+                    .to_vec()
+                    .iter()
+                    .map(|(a, b)| (a.to_vec(), b.to_vec()))
+                    .collect::<Vec<_>>();
 
-            let out = outbuf.iter().take_while(|(f, s)| s.is_empty()).map(|(a, b)| a.clone());
-            //while (outbuf.front())
-            server_write_tls.write_all(&testbuf).await;
+                for (c, _) in &outbuf {
+                    //println!("{:?}", candidates);
+                    candidates = candidates
+                        .into_iter()
+                        .filter(|(a, _)| a.first() == Some(c))
+                        .collect::<Vec<_>>();
+                    if candidates.len() == 1 {
+                        break;
+                    }
+                }
+
+                let candidate = candidates.first().unwrap();
+                //println!("DETECTED: {:?}", candidates.first().unwrap().1.iter().map(|&i| i as char).collect::<Vec<_>>());
+
+                //Replace byte stream
+                outbuf.drain(0..candidate.0.len().clone());
+
+                let mut oldoutbuf = outbuf;
+
+                outbuf = candidate.to_owned().1.into_iter().map(|f| (f, Vec::new())).collect::<VecDeque<_>>();
+                outbuf.append(&mut oldoutbuf);
+            }
+
+            //write unmatched bytes to output stream
+            let out = outbuf
+                .iter()
+                .take_while(|(_, s)| s.is_empty())
+                .map(|(a, _)| a.clone())
+                .collect::<Vec<_>>();
+
+            outbuf.drain(0..out.len());
+
+
+            out.iter().for_each(|&f| print!("{}", (f as char).to_string().green()));
+            server_write_tls.write_all(&out).await;
         }
     });
 
@@ -111,7 +161,7 @@ async fn handle_client(tls_stream_client: TlsStream<TcpStream>) {
                 println!("Outgoing thread reading failed, terminating thread.");
                 return;
             }
-            //print!("{}", testbuf[0] as char);
+            print!("{}", (testbuf[0] as char).to_string().cyan());
             client_write_tls.write_all(&testbuf).await;
         }
     });
