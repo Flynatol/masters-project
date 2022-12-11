632239ec104cbd8f1a6df9fef69c0bf70e488d51
Flynn McGouran-Collins <fmm99@student.bham.ac.uk>
2022-12-11 06:10:20 +0000

    Moved colours to their own file

---
+++ src/colours.rs
@@ -0,0 +1,104 @@
+use colored::Color;
+
+pub static COLOURS: &'static [Color] = &[
+    Color::TrueColor {
+        r: 255,
+        g: 179,
+        b: 0,
+    },
+    Color::TrueColor {
+        r: 128,
+        g: 62,
+        b: 117,
+    },
+    Color::TrueColor {
+        r: 255,
+        g: 104,
+        b: 0,
+    },
+    Color::TrueColor {
+        r: 166,
+        g: 189,
+        b: 215,
+    },
+    Color::TrueColor {
+        r: 193,
+        g: 0,
+        b: 32,
+    },
+    Color::TrueColor {
+        r: 206,
+        g: 162,
+        b: 98,
+    },
+    Color::TrueColor {
+        r: 129,
+        g: 112,
+        b: 102,
+    },
+    Color::TrueColor {
+        r: 0,
+        g: 125,
+        b: 52,
+    },
+    Color::TrueColor {
+        r: 246,
+        g: 118,
+        b: 142,
+    },
+    Color::TrueColor {
+        r: 0,
+        g: 83,
+        b: 138,
+    },
+    Color::TrueColor {
+        r: 255,
+        g: 122,
+        b: 92,
+    },
+    Color::TrueColor {
+        r: 83,
+        g: 55,
+        b: 122,
+    },
+    Color::TrueColor {
+        r: 255,
+        g: 142,
+        b: 0,
+    },
+    Color::TrueColor {
+        r: 179,
+        g: 40,
+        b: 81,
+    },
+    Color::TrueColor {
+        r: 244,
+        g: 200,
+        b: 0,
+    },
+    Color::TrueColor {
+        r: 127,
+        g: 24,
+        b: 13,
+    },
+    Color::TrueColor {
+        r: 147,
+        g: 170,
+        b: 0,
+    },
+    Color::TrueColor {
+        r: 89,
+        g: 51,
+        b: 21,
+    },
+    Color::TrueColor {
+        r: 241,
+        g: 58,
+        b: 19,
+    },
+    Color::TrueColor {
+        r: 35,
+        g: 44,
+        b: 22,
+    },
+];
