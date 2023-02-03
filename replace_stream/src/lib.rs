2d2d1871717d37447730e74e971a69e38defb96a
Flynn McGouran-Collins <fmm99@student.bham.ac.uk>
2023-02-03 20:23:01 +0000

    Finally pushed replayer to git, refactored code to use shared crate

---
+++ replace_stream/src/lib.rs
@@ -0,0 +1,162 @@
+pub mod replace_mod {
+    use bytes::Bytes;
+    use std::collections::VecDeque;
+    use std::pin::Pin;
+    use std::sync::mpsc;
+    use tokio_util::io::ReaderStream;
+    use std::sync::Arc;
+
+    pub struct ReplaceStream<T: tokio_stream::Stream + std::marker::Unpin> {
+        stream: T,
+        pub buffer: VecDeque<u8>,
+        pub triggers: Vec<(Vec<u8>, Arc<dyn Fn(&mut ReplaceStream<T>) -> () + Send + Sync>)>,
+    }
+
+    pub fn replacment_builder<'a, T: tokio::io::AsyncRead + std::marker::Unpin>(
+        stream: T,
+        triggers: Vec<(
+            Vec<u8>,
+            Arc<dyn Fn(&mut ReplaceStream<ReaderStream<T>>) -> () + Send + Sync>,
+        )>,
+    ) -> ReplaceStream<ReaderStream<T>> {
+        ReplaceStream {
+            stream: ReaderStream::new(stream),
+            buffer: VecDeque::new(),
+            triggers,
+        }
+    }
+
+    impl<T: tokio_stream::Stream<Item = Result<Bytes, std::io::Error>> + std::marker::Unpin>
+        tokio_stream::Stream for ReplaceStream<T>
+    {
+        type Item = u8;
+
+        fn poll_next(
+            self: Pin<&mut Self>,
+            cx: &mut std::task::Context<'_>,
+        ) -> std::task::Poll<Option<u8>> {
+            let mut add_to_buffer: VecDeque<u8> = VecDeque::new();
+            let sref = self.get_mut();
+            let mut matches;
+            
+            let mut loc_triggers = sref
+                .triggers
+                .iter()
+                .map(|(a, b)| (&a[..], b))
+                .collect::<Vec<_>>();
+
+            //print!("triggers len: {}    ", loc_triggers.len());
+            loop {
+                let read = match sref.buffer.is_empty().to_owned() {
+                    true => {
+                        match Pin::new(&mut sref.stream).poll_next(cx) {
+                            std::task::Poll::Ready(result) => {
+                                match result {
+                                    Some(b) => {
+                                        //write bytes into buffer
+                                        let mut bytes = b
+                                            .expect("Some contains error")
+                                            .iter()
+                                            .map(|&f| f)
+                                            .collect::<VecDeque<u8>>();
+                                        sref.buffer.append(&mut bytes);
+                                        sref.buffer.pop_front().unwrap()
+                                    }
+                                    None => {
+                                        //We have reached EOF
+                                        add_to_buffer.append(&mut sref.buffer);
+                                        sref.buffer = add_to_buffer;
+                                        println!("Disconnected, stream retuned EOF");
+                                        return std::task::Poll::Ready(None);
+                                    }
+                                }
+                            }
+                            std::task::Poll::Pending => {
+                                //We're waiting on the underlying stream
+                                add_to_buffer.append(&mut sref.buffer);
+                                sref.buffer = add_to_buffer;
+                                return std::task::Poll::Pending;
+                            }
+                        }
+                    }
+                    false => {
+                        //If buffer is available read from that instead
+                        sref.buffer.pop_front().unwrap()
+                    }
+                };
+
+
+                add_to_buffer.push_back(read);
+
+                loc_triggers = loc_triggers
+                    .into_iter()
+                    .filter(|(x, _)| x.first() == Some(&read))
+                    .map(|(a, b)| (&a[1..], b))
+                    .collect::<Vec<_>>();
+
+                matches = loc_triggers.iter().filter(|(x, _)| x.is_empty()).next();
+
+                //println!("Read: {:02x?}, matches: {}", read, loc_triggers.len());
+
+                if matches.is_some() || loc_triggers.is_empty() {
+                    break;
+                }
+            }
+            
+            add_to_buffer.append(&mut sref.buffer);
+            sref.buffer = add_to_buffer;
+
+            //This bit is dumb but I can't see how else to please the borrow checker
+            let mut c = None;
+            if let Some(&(_a, b)) = matches {
+                c = Some(b.clone());
+            }
+
+            match c {
+                Some(a) => (a)(sref),
+                None => {},
+            }
+
+            //if let c = Some(a) {
+            //    (a)(sref);
+            //}
+             
+
+            
+            //let res = sref.buffer.pop_front();
+            std::task::Poll::Ready(sref.buffer.pop_front())
+        }
+    }
+
+    impl<T: tokio_stream::Stream<Item = Result<Bytes, std::io::Error>> + std::marker::Unpin>
+        ReplaceStream<T>
+    {
+        pub fn replace(&mut self, target: &[u8], repl: &[u8]) {
+            self.buffer.drain(0..target.len());
+            let mut new = VecDeque::from(repl.to_vec());
+            new.append(&mut self.buffer);
+            self.buffer = new;
+        }
+
+        pub fn replace_vec(&mut self, target: &Vec<u8>, repl: &Vec<u8>) {
+            println!("replacment triggered");
+            self.buffer.drain(0..target.len());
+            let mut new = VecDeque::from(repl.clone());
+            new.append(&mut self.buffer);
+            self.buffer = new;
+        }
+
+        pub fn message<S : Copy + Send + Sync + 'static>(sender: mpsc::SyncSender<S>, message: S) -> Arc<dyn Fn(&mut ReplaceStream<T>) -> () + Send + Sync> {
+            println!("message");
+            //sender.send(message);
+            Arc::new(move |_x| {sender.send(message);})
+        }
+
+
+        pub fn rpl_boxed (target: Vec<u8>, repl: Vec<u8>) -> Arc<dyn Fn(&mut ReplaceStream<T>) -> () + Send + Sync> {
+            //Box::new(move |x| x.replace_vec(&target, &repl))
+            Arc::new(move|x| x.replace_vec(&target, &repl))
+
+        }
+    }
+}
---
+++ replay/src/main.rs
@@ -0,0 +1,169 @@
+use native_tls::Identity;
+use replace_stream::replace_mod::ReplaceStream;
+use tokio_stream::StreamExt;
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::fs;
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt, ReadHalf};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_native_tls::{TlsAcceptor, TlsConnector, TlsStream};
+use colored::{Colorize, Color};
+use std::io::{stdin, stdout, Read, Write};
+use std::ffi::OsStr;
+use itertools::{interleave, Itertools};
+use bytes::Bytes;
+use tokio_util::io::ReaderStream;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+use replace_stream::replace_mod::replacment_builder;
+
+
+const TARGET : &str = "192.168.121.98";
+const PORT : &str = ":41100";
+const MY_PORT: &str = ":41100";
+const MY_IP : &str = "192.168.121.144"; //TODO grab this automatically
+const REPLACEMENTS: &'static [(&[u8], &[u8])] = &[(MY_IP.as_bytes(), TARGET.as_bytes()),
+												  ("192.168.zz".as_bytes(), MY_IP.as_bytes()),
+                                                  (TARGET.as_bytes(), MY_IP.as_bytes()),
+												  ("www.wikipedia.org".as_bytes(), MY_IP.as_bytes()),
+                                                  ("fflkskkk".as_bytes(), "W2113dsf".as_bytes())];
+
+#[tokio::main]
+async fn main() {
+    let connector: TlsConnector = TlsConnector::from(
+		native_tls::TlsConnector::builder()
+			.danger_accept_invalid_certs(true)
+			.danger_accept_invalid_hostnames(true)
+			.build()
+			.unwrap(),
+    );
+
+	//clean up logs
+	fs::read_dir("./logs").unwrap()
+		.filter(|f| f.as_ref().unwrap().path().extension() == Some(OsStr::new("log")))
+		.for_each(|f| { fs::rename(f.as_ref().unwrap().path().to_str().unwrap(), format!("{}.old", f.unwrap().path().to_str().unwrap())); });
+
+	let test = tokio::fs::File::open("payload_program.log").await.expect("Could not read file");
+
+	let (tx, rx) = mpsc::sync_channel(512);
+
+	//let cls = |f : &mut ReplaceStream<ReaderStream<tokio::io::ReadHalf<_>>>| 
+	//	f.message(tx, Signal::Key);
+	//let cls = rpl_boxed(vec![], repl)
+
+	//let test2 = ReplaceStream::<'_, ReaderStream<tokio::fs::File>>::message(tx, Signal::Key);
+
+	let mut file_stream = replacment_builder(test, vec![
+		//(&[0xe8, 0xbf, 0x27, 0x09], test2),
+		//("Dieses".as_bytes(), (&|f| f.replace("Dieses".as_bytes(), "tested".as_bytes()))),
+	]);
+
+	
+
+	
+
+    let stream_out = TcpStream::connect(format!("{}{}", TARGET, PORT)).await.unwrap();
+    let mut tls_stream_server = connector
+        .connect("googlasde.com", stream_out)
+        .await
+        .unwrap();
+
+
+
+	let (server_read_tls, mut server_write_tls) = io::split(tls_stream_server);
+
+	let message_fn = ReplaceStream::<ReaderStream<ReadHalf<TlsStream<TcpStream>>>>::message(tx, Signal::Key);
+
+	let mut server_read_tls = replacment_builder(server_read_tls, vec![
+		([0xe8, 0xbf, 0x27, 0x09].to_vec(), message_fn),
+			//("Dieses".as_bytes(), (&|f| f.replace("Dieses".as_bytes(), "tested".as_bytes()))),
+	]);	
+	
+	while let Some(tag) = file_stream.next().await {
+		if tag == 0xff {
+			println!("read ff tag");
+			break;
+		};
+		let data = file_stream.next().await.expect("File stream ended unexpectedly");
+		if tag % 2 == 0  {
+			server_write_tls.write_u8(data).await.unwrap();
+			print!("{}", format!(" {:02x?}", data).color(Color::TrueColor {r : 193,  g : 0,   b : 32}));
+		} else {
+			let rec = server_read_tls.next().await.expect("No data recived");
+
+			if let Ok(x) = rx.try_recv() {
+				println!("SIGNAL");
+				//Do the buffer modifications here
+				//Skip as many bytes in the file as we consume from stream to keep them in sync.
+				//server_read_tls = server_read_tls.skip(4);
+				
+				let mut trg = vec![];
+
+				for _ in (0..3) {
+					//server_read_tls.next().await;
+					println!("skp: {:02x?}", server_read_tls.next().await.unwrap())
+				}
+
+				for _ in (0..4) {
+					let byte = server_read_tls.next().await.unwrap();
+					println!("trg {:02x?}", byte);
+					trg.push(0x04);
+					trg.push(byte);
+					
+					//trg.push(byte);
+				}
+				
+				let mut to_replace = vec![];
+
+				for i in (0..7) {
+					let v = file_stream.next().await.unwrap();
+					println!("skp f: {:02x?}", v);
+					if i % 2 == 1 {
+						//to_replace.push(v)
+					}
+				}
+
+				for i in (0..7) {
+					let v = file_stream.next().await.unwrap();
+					println!("skp f: {:02x?}", v);
+					if i % 2 == 0 {
+						to_replace.push(0x04);
+						to_replace.push(v)
+					}
+				}
+
+				println!("Pushing replacement of {:02x?} to {:02x?}", &to_replace, &trg);
+				let lmb = ReplaceStream::rpl_boxed(to_replace.clone(), trg);
+				file_stream.triggers.push((to_replace, lmb));
+
+			}
+
+			if rec == data {
+				print!("{}", format!(" {:02x?}", data).color(Color::TrueColor {r : 206,  g : 162, b : 98}));
+			} else {
+				print!("[Rc : {:02x?}, Exp: {:02x?}]", rec, data);
+			}
+		}
+	}
+	let now = Instant::now();
+	server_write_tls.write_u8(0xff).await;
+
+	server_read_tls.next().await;
+	let elapsed_time = now.elapsed();
+
+	println!("Response took {}", elapsed_time.as_micros());
+
+
+	while let Some(x) = server_read_tls.next().await {
+		print!("{}", format!(" {:02x?}", x).color(Color::TrueColor {r : 206,  g : 162, b : 98}));
+		std::io::stdout().flush().unwrap()
+	}
+	
+	
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Signal{Key}
